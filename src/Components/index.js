31dc19a8ae5a5ea8dc86dfa58885af4e3bc216e1
Pranjal <patelpranjal802@gmail.com>
2025-03-28 18:00:35 +0530

    Made component for sign up with validation and also madde a component for Real Time Editor(RTE) and also used useForm hook for getting inputs Made a form for posting a blog and made authentication layout

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,5 +6,6 @@ import LogoutBtn from './Header/LogoutBtn'
 import Button from '../Components/Button'
 import Input from '../Components/Input'
 import Select from '../Components/Select'
+import RTE from './RTE'
  
-export {Header,Footer,Container,Logo,LogoutBtn,Button,Input,Select}
+export {Header,Footer,Container,Logo,LogoutBtn,Button,Input,Select,RTE}
